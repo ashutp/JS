3bdd7da6244c5f607fd612e6fbef2cbd8f83f836
Ashutosh Kaithal <149809006+ashutp@users.noreply.github.com>
2025-11-19 09:45:02 +0000

    data types

---
+++ DATA-TYPES/Types.js
@@ -0,0 +1,8 @@
+console.log("There are two types of data types in js one is Preimitve and second is non-primitve datatype:\n ");
+console.log("defferance of both of them is that primitve can be copy with value and non-primitve copy by referance.\n");
+console.log("primitve can immutable and non-primitve is  mutable.\n");
+
+console.log("Primitve data types are : number,string,boolean,null,undefiend,symbol,Bigint.\n");
+console.log("Non-primitve data types are: obj,function,array.");
+
+
